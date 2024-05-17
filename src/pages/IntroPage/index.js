ac113dd10ec9b5bcdb9960a3aa03ed4006490560
Mohammad Fardin Rahman <fardinr.24@gmail.com>
2024-05-17 19:02:11 -0400

    URL Paths added for Login and Signup Pages

--- src/pages/IntroPage/index.js
+++ src/pages/IntroPage/index.js
@@ -1,15 +1,17 @@
 import * as React from 'react';
+import {Link} from 'react-router-dom';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+
 
 
 let theme = createTheme({
@@ -19,7 +21,7 @@ let theme = createTheme({
     palette: {
       roktoLaal: theme.palette.augmentColor({
         color: {
-          main: '#FF0000',
+          main: '#e30909',
         },
         name: 'roktoLaal',
       }),
@@ -30,9 +32,9 @@ function Copyright(props) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
-      <Link color="inherit" href="#">
+      <a color="inherit" href="#">
         T Bank Inc.
-      </Link>{' '}
+      </a>{' '}
       {new Date().getFullYear()}
       {'.'}
     </Typography>
@@ -42,6 +44,8 @@ function Copyright(props) {
 
 
 function IntroductionPage() {
+  
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
@@ -50,6 +54,7 @@ function IntroductionPage() {
       password: data.get('password'),
     });
   };
+  
 
   return (
     <ThemeProvider theme={theme}>
@@ -79,7 +84,7 @@ function IntroductionPage() {
               alignItems: 'center',
             }}
           >
-            <Avatar sx={{ m: 1, bgcolor: 'red' }}>
+            <Avatar sx={{ m: 4, bgcolor: 'red' }}>
               <LockOutlinedIcon />
             </Avatar>
             <Typography component="h1" variant="h5">
@@ -117,14 +122,11 @@ function IntroductionPage() {
               </Button>
               <Grid container>
                 <Grid item xs>
-                  <Link href="#" variant="body2">
-                    Forgot password?
-                  </Link>
+                  
                 </Grid>
                 <Grid item>
-                  <Link onClick='./pages/CreateAccount/index.js' variant="body2">
-                    {"Don't have an account? Sign Up"}
-                  </Link>
+                  <Link to='/Signup'>Don't have an account? Signup</Link>
+                
                 </Grid>
               </Grid>
               <Copyright sx={{ mt: 5 }} />
